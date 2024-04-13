5e315854b9b13d204a15ce7ebb134ba3858fffed
khomyakdi <khomyakdi@gmail.com>
2024-04-13 15:53:09 +0300

    Adjust layout components

--- timer.py
+++ timer.py
@@ -1,5 +1,29 @@
-from guizero import App, Text, PushButton
+from guizero import App, Text, PushButton, Box
 
-app = App(title="Timer")
+def reset():
+    print("reset")
+
+def timer25():
+    print("25")
+
+def timer5():
+    print("5")    
+
+def timer15():
+    print("15")
+
+app = App(title="Timer", width=300, height=150)
+
+timer_box = Box(app, width="fill", align="top", )
+timer_text = Text(timer_box, align="left", text="00:00", size=40)
+
+counter_box = Box(timer_box, align="right")
+counter_text = Text(counter_box, align="left", text="0", size=30)
+rst_btn = PushButton(counter_box, align="right", text="RST")
+
+control_box = Box(app, layout="grid", align="bottom")
+timer25_btn = PushButton(control_box, grid=[0,0], text="25", padx=30, pady=20)
+timer5_btn = PushButton(control_box, grid=[1,0], text="5", padx=30, pady=20, command=timer5)
+timer15_btn = PushButton(control_box, grid=[2,0], text="15", padx=30, pady=20)
 
 app.display()
